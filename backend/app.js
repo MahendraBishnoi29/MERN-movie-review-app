3e86edc1522fe3f132f323258e6af9696a4f88eb
Mahendra <mahendrabrowny@gmail.com>
2022-07-31 18:51:29 +0530

    files imported and required in main server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+require("./db");
+const userRouter = require("./routes/user");
 
 const app = express();
+app.use(express.json());
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Welcome Page");
