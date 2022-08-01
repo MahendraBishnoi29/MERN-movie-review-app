9a9e3a6af0c82a490fa1d3846dbca754b65181ba
Mahendra <mahendrabrowny@gmail.com>
2022-08-01 15:04:36 +0530

    refactoring

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,6 @@ const app = express();
 app.use(express.json());
 app.use("/api/user", userRouter);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Welcome Page");
-});
-
 app.listen(8000, () => {
   console.log("Server is Running");
 });
