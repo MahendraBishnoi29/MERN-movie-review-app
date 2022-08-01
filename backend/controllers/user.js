53f98d34df2eedb26f2b65f1c640903a1bf1c21b
Mahendra <mahendrabrowny@gmail.com>
2022-08-01 15:05:20 +0530

    user signup method with hashed password 🚀

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,7 +2,14 @@ const User = require("../models/userModel");
 
 exports.createUser = async (req, res) => {
   const { name, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res
+      .status(401)
+      .json({ error: "A User With That Email Already Exist!!" });
+  }
   const newUser = new User({ name, email, password });
   await newUser.save();
-  res.json({ user: newUser });
+  res.status(201).json({ user: newUser });
 };
