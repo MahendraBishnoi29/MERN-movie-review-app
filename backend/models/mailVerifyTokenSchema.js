96aa90d8a0d6622e59456ca2ab136b9cce52d05e
Mahendra <mahendrabrowny@gmail.com>
2022-08-03 13:51:44 +0530

    email token verification

---
+++ backend/models/mailVerifyTokenSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const emailVerifyTokenSchema = mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    expires: 3600,
+    default: Date.now(),
+  },
+});
+
+emailVerifyTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    this.token = await bcrypt.hash(this.token, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model(
+  "EmailVerificationToken",
+  emailVerifyTokenSchema
+);
