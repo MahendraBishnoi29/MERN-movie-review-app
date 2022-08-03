51ff0d94b39422b66eab01bc417a013d65e7c798
Mahendra <mahendrabrowny@gmail.com>
2022-08-03 12:13:14 +0530

    error handling with try catch

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,15 +1,21 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 exports.createUser = async (req, res) => {
   const { name, email, password } = req.body;
   const existingUser = await User.findOne({ email });
-
   if (existingUser) {
     return res
       .status(401)
       .json({ error: "A User With That Email Already Exist!!" });
   }
-  const newUser = new User({ name, email, password });
-  await newUser.save();
+
+  const hashedPassword = bcrypt.hashSync(password);
+  const newUser = new User({ name, email, password: hashedPassword });
+  try {
+    await newUser.save();
+  } catch (err) {
+    console.log("error creating user", err.message);
+  }
   res.status(201).json({ user: newUser });
 };
