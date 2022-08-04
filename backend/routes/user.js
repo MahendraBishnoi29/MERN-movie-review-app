59e84c9af6abbe6355312fb8e09efab63ed6c5a9
Mahendra <mahendrabrowny@gmail.com>
2022-08-04 14:02:31 +0530

    route for verifying user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/user");
+const { createUser, verifyEmail } = require("../controllers/user");
 const { Validator, validateUser } = require("../middlewares/validator");
 
 router.post("/create", Validator, validateUser, createUser);
+router.post("/verify-email", verifyEmail);
 
 module.exports = router;
