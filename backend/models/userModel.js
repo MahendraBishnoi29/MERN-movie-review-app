8fb258abddbb8503505218d72f83a0e3642918f4
Mahendra <mahendrabrowny@gmail.com>
2022-08-04 14:02:58 +0530

    type for checking user verification

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,11 @@ const userSchema = mongoose.Schema({
   name: { type: String, trim: true, required: true },
   email: { type: String, trim: true, required: true, unique: true },
   password: { type: String, required: true },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
