c30cd66c00cdce23c874c4780db688861edbbbd4
Mahendra <mahendrabrowny@gmail.com>
2022-08-04 14:02:16 +0530

    function for verifying email is ✔

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,8 +2,9 @@ const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const mailVerifyTokenSchema = require("../models/mailVerifyTokenSchema");
 const nodemailer = require("nodemailer");
+const { isValidObjectId } = require("mongoose");
 
-// Register User
+// Register User Function
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
   const existingUser = await User.findOne({ email });
@@ -13,6 +14,7 @@ const createUser = async (req, res) => {
       .json({ error: "A User With That Email Already Exist!!" });
   }
 
+  // Hash Password
   const hashedPassword = bcrypt.hashSync(password);
   const newUser = new User({ name, email, password: hashedPassword });
 
@@ -29,7 +31,7 @@ const createUser = async (req, res) => {
     OTP += randomVal;
   }
 
-  //Store OTP inside our DB
+  //Store OTP & Token inside our DB
   const newEmailVerifyToken = new mailVerifyTokenSchema({
     owner: newUser._id,
     token: OTP,
@@ -48,7 +50,7 @@ const createUser = async (req, res) => {
   });
 
   transport.sendMail({
-    from: "verification@movieapp.com",
+    from: "verify@movieapp.com",
     to: newUser.email,
     subject: "Email Verification",
     html: `<p>Your OTP for Email verification</P>
@@ -56,9 +58,56 @@ const createUser = async (req, res) => {
     `,
   });
 
-  res.status(201).json({ user: "OTP has sent to your mail" });
+  res.status(201).json({ user: "User is Created & OTP has sent to mail" });
 };
 
-// Login User
+// Function For Verifying Email & UserID Token
+const verifyEmail = async (req, res) => {
+  const { userId, OTP } = req.body;
+  if (!isValidObjectId(userId)) return res.json({ error: "Invalid User ID" });
 
-module.exports = { createUser };
+  const user = await User.findById(userId);
+  if (!user) return res.json({ error: "User Not Found!" });
+  if (user.isVerified) return res.json({ error: "User is Already Verified!" });
+
+  const token = await mailVerifyTokenSchema.findOne({ owner: userId });
+  if (!token) return res.json({ error: "Token Not Found!" });
+
+  const isMatched = await token.compareToken(OTP);
+  if (!isMatched) return res.json({ error: "Please Enter a Valid OTP!" });
+
+  user.isVerified = true;
+  await user.save();
+
+  await mailVerifyTokenSchema.findByIdAndDelete(token._id);
+
+  const newEmailVerifyToken = new mailVerifyTokenSchema({
+    owner: user._id,
+    token: OTP,
+  });
+
+  await newEmailVerifyToken.save();
+
+  // Send OTP to Email Account
+  var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "7db3d4874b68dd",
+      pass: "ce5e5966e5f191",
+    },
+  });
+
+  transport.sendMail({
+    from: "verify@movieapp.com",
+    to: user.email,
+    subject: "Welcome",
+    html: `<h2>Welcome To MERN Movie Review App 🤗</h2>`,
+  });
+
+  res.json({ message: "Your Email is Verified " });
+};
+
+// Login User Function
+
+module.exports = { createUser, verifyEmail };
