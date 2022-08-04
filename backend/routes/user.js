f355f4bfd090f9024a1e906575c191dc42065e81
Mahendra <mahendrabrowny@gmail.com>
2022-08-04 14:59:11 +0530

    route for resending verification OTP

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, verifyEmail } = require("../controllers/user");
+const {
+  createUser,
+  verifyEmail,
+  resendEmailVerificationToken,
+} = require("../controllers/user");
 const { Validator, validateUser } = require("../middlewares/validator");
 
 router.post("/create", Validator, validateUser, createUser);
 router.post("/verify-email", verifyEmail);
+router.post("/resend-email-verifyToken", resendEmailVerificationToken);
 
 module.exports = router;
