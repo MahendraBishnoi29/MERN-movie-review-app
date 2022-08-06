95774167354a226847ec1066eb39c191e528b1bd
Mahendra <mahendrabrowny@gmail.com>
2022-08-06 15:40:32 +0530

    route for resetting password

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,11 +4,13 @@ const {
   createUser,
   verifyEmail,
   resendEmailVerificationToken,
+  forgetPassword,
 } = require("../controllers/user");
 const { Validator, validateUser } = require("../middlewares/validator");
 
 router.post("/create", Validator, validateUser, createUser);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-email-verifyToken", resendEmailVerificationToken);
+router.post("/reset-password", forgetPassword);
 
 module.exports = router;
