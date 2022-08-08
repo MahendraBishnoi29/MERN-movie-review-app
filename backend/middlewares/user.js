e7b858f1a2d4af8aeeb8983007e7745de1bd36ed
Mahendra <mahendrabrowny@gmail.com>
2022-08-08 14:51:46 +0530

    passerod reset middleware

---
+++ backend/middlewares/user.js
@@ -0,0 +1,23 @@
+const { isValidObjectId } = require("mongoose");
+const passwordResetModel = require("../models/passwordResetModel");
+
+const isValidPassResetToken = async (req, res, next) => {
+  const { token, userId } = req.body;
+
+  if (!token.trim() || !isValidObjectId(userId))
+    return res.json({ error: "Invalid Request" });
+
+  const resetToken = await passwordResetModel.findOne({ owner: userId });
+  if (!resetToken)
+    return res.json({ error: "Unauthorized access, invalid request!" });
+
+  const matched = await resetToken.compareToken(token);
+  if (!matched)
+    return res.json({ error: "Unauthorized access, Invalid request" });
+
+  req.resetToken = resetToken;
+
+  next();
+};
+
+module.exports = isValidPassResetToken;
