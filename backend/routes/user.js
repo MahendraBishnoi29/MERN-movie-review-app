6fcb96e0d4cd9c185d4522e72df8321990afedb5
Mahendra <mahendrabrowny@gmail.com>
2022-08-08 14:50:59 +0530

    verify password reset token router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,11 +6,15 @@ const {
   resendEmailVerificationToken,
   forgetPassword,
 } = require("../controllers/user");
+const isValidPassResetToken = require("../middlewares/user");
 const { Validator, validateUser } = require("../middlewares/validator");
 
 router.post("/create", Validator, validateUser, createUser);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-email-verifyToken", resendEmailVerificationToken);
 router.post("/reset-password", forgetPassword);
+router.post("/password-reset-token", isValidPassResetToken, (req, res) => {
+  res.json({ valid: true });
+});
 
 module.exports = router;
