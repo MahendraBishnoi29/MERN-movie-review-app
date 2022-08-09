ca45a37b096849504419f22a6675e8d831d4b10b
Mahendra <mahendrabrowny@gmail.com>
2022-08-09 15:18:22 +0530

    important credentials replaced with .env

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/review_app")
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB Connected 🚀");
   })
