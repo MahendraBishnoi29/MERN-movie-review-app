11702537b7a664919521c25f3a3b118eb2befef6
Mahendra <mahendrabrowny@gmail.com>
2022-08-09 13:43:55 +0530

    functions for hashing and comparing the password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   name: { type: String, trim: true, required: true },
@@ -10,5 +11,17 @@ const userSchema = mongoose.Schema({
     default: false,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  const result = await bcrypt.compare(password, this.password);
+  return result;
+};
 
 module.exports = mongoose.model("User", userSchema);
