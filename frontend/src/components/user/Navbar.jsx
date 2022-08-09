7204c63b0ed52d2dc0d37c41d9e9cbc52ac3f50e
Mahendra <mahendrabrowny@gmail.com>
2022-08-09 22:04:57 +0530

    Navbar component

---
+++ frontend/src/components/user/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-secondary">
+      <div className="bg-primary text-white  max-w-screen-xl mx-auto">
+        Hello
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
