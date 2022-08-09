d1e687b9fa302d8dc3bfd98e5fb7b96808881f90
Mahendra <mahendrabrowny@gmail.com>
2022-08-09 13:44:15 +0530

    validator for validate newPassword

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -1,5 +1,6 @@
 const { check, validationResult } = require("express-validator");
 
+// Validate Complete User With All Fields
 exports.Validator = [
   check("name").trim().not().isEmpty().withMessage("Name is missing!"),
   check("email").normalizeEmail().isEmail().withMessage("Email is Invalid!!"),
@@ -12,7 +13,19 @@ exports.Validator = [
     .withMessage("Password must be 6 to 15 characters long!"),
 ];
 
-exports.validateUser = (req, res, next) => {
+// Validate Password Only
+exports.validatePassword = [
+  check("newPassword")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Please Enter Your New Password!")
+    .isLength({ min: 6, max: 15 })
+    .withMessage("Password must be 6 to 15 characters long!"),
+];
+
+// For formating Error Message
+exports.validate = (req, res, next) => {
   const error = validationResult(req).array();
   if (error.length) {
     return res.json({ error: error[0].msg });
