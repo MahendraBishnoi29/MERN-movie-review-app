e4c6f641f183600fe2b385fcd5b04c4c8f445120
Mahendra <mahendrabrowny@gmail.com>
2022-08-09 15:20:00 +0530

    basic changes 🐛

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
+require("dotenv").config();
 require("./db");
 const userRouter = require("./routes/user");
 
 const app = express();
 app.use(express.json());
+
 app.use("/api/user", userRouter);
+
+app.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "No User Found Please SignUp first" });
+  }
+  next();
+});
 
 app.listen(8000, () => {
   console.log("Server is Running");
