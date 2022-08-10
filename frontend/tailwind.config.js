3a1519f3789b0b8a3d4319c0f8305c283a6ee2a5
Mahendra <mahendrabrowny@gmail.com>
2022-08-10 13:31:19 +0530

    tailwind css config 🛠

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extends: {
+    extend: {
       colors: {
         primary: "#171717",
         secondary: "#272727",
-        "dark-subtle": "#rgba(255, 255, 255, 0.5)",
-        "light-subtle": "#rgba(39, 39, 39, 0.5)",
+        "dark-subtle": "rgba(255, 255, 255, 0.5)",
+        "light-subtle": "rgba(39, 39, 39, 0.5)",
       },
     },
   },
