3d6c3e3e1ed3c328651ac85b59af31a9d4da0e63
Mahendra <mahendrabrowny@gmail.com>
2022-08-10 18:21:27 +0530

    all routes with components done 🎉

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
-// import SignIn from "./components/Auth/signIn/SignIn";
-import SignUp from "./components/Auth/signUp/SignUp";
+import Navbar from "./components/Navbar/Navbar";
+import SignIn from "./components/Auth/SignIn";
+import SignUp from "./components/Auth/SignUp";
+import EmailVerification from "./components/Auth/EmailVerification";
+import Home from "./components/Home/Home";
+import ForgetPassword from "./components/Auth/ForgetPassword";
+import ConfirmPassword from "./components/Auth/ConfirmPassword";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <SignUp />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/email-verification" element={<EmailVerification />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/confirm-password" element={<ConfirmPassword />} />
+      </Routes>
     </>
   );
 }
