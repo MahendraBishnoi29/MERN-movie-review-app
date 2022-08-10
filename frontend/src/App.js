d6728d7eec2340f54a4c2c1ea3e9401c207f8a8f
Mahendra <mahendrabrowny@gmail.com>
2022-08-10 16:19:42 +0530

    rendering signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import SignIn from "./components/Auth/signIn/SignIn";
+// import SignIn from "./components/Auth/signIn/SignIn";
+import SignUp from "./components/Auth/signUp/SignUp";
 
 function App() {
   return (
     <>
       <Navbar />
-      <SignIn />
+      <SignUp />
     </>
   );
 }
