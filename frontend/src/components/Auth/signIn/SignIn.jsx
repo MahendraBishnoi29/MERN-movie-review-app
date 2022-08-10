dc7d31c2f8b9dce2ad270c5d35f2d34c8b86c7b6
Mahendra <mahendrabrowny@gmail.com>
2022-08-10 15:57:39 +0530

    sign in form refactored 🚀

--- frontend/src/components/Auth/signIn/SignIn.jsx
+++ frontend/src/components/Auth/signIn/SignIn.jsx
@@ -1,28 +1,16 @@
 import React from "react";
+import Input from "../../Form/Input";
+import Title from "../../Form/Title.tsx";
 import Container from "../../navbar/Container";
 
 const SignIn = () => {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
       <Container>
-        <form className="bg-secondary rounded p-6 w-72">
-          <h1 className="text-xl text-white font-semibold text-center">
-            SignIn 🔑
-          </h1>
-          <div className="flex flex-col-reverse">
-            <input
-              type="text"
-              id="email"
-              className="text-white outline-none bg-transparent rounded border-2 border-dark-subtle  w-full focus:border-white p-1 transition peer"
-              placeholder="John@gmail.com"
-            />
-            <label
-              className="font-semibold text-dark-subtle peer-focus:text-white self-start mb-1"
-              htmlFor="email"
-            >
-              Email
-            </label>
-          </div>
+        <form className="bg-secondary rounded space-y-4 p-6 w-72">
+          <Title>SignIn 🔑</Title>
+          <Input name="email" label="Email" placeholder="johnwick@gmail.com" />
+          <Input name="password" label="Password" placeholder="********" />
         </form>
       </Container>
     </div>
