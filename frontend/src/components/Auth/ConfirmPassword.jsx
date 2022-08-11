f339713e02538cc9d7f2c51dd866bb23dee32972
Mahendra <mahendrabrowny@gmail.com>
2022-08-11 21:32:46 +0530

    confirm password component created

--- frontend/src/components/Auth/ConfirmPassword.jsx
+++ frontend/src/components/Auth/ConfirmPassword.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import CustomLink from "../CustomLink/CustomLink";
+import Input from "../Form/Input";
+import Submit from "../Form/Submit";
+import Title from "../Form/Title";
+import Container from "../Navbar/Container";
 
 const ConfirmPassword = () => {
-  return <div>ConfirmPassword</div>;
+  return (
+    <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded space-y-4 p-6 w-80">
+          <Title>Enter New Password</Title>
+          <Input
+            name="password"
+            label="New Password"
+            placeholder="new password"
+            type="password"
+          />
+          <Input
+            name="confirmPassword"
+            label="Confirm New Password"
+            placeholder="confirm password"
+            type="password"
+          />
+          <Submit value="Save Password" />
+        </form>
+      </Container>
+    </div>
+  );
 };
 
 export default ConfirmPassword;
