c64818d69ac378ea46dac50aed04fb867d2639e4
Mahendra <mahendrabrowny@gmail.com>
2022-08-12 15:45:42 +0530

    basic states and input setup done

--- frontend/src/components/Auth/SignUp.jsx
+++ frontend/src/components/Auth/SignUp.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import { useState } from "react";
 import { commonModalClasses } from "../../utils/theme";
 import CustomLink from "../CustomLink/CustomLink";
 import FormContainer from "../Form/formContainer/FormContainer";
@@ -9,14 +10,51 @@ import Title from "../Form/Title";
 import Container from "../Navbar/Container";
 
 const SignUp = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+  };
+
+  const { name, email, password } = userInfo;
+
   return (
     <FormContainer>
       <Container>
-        <form className={commonModalClasses + " w-72"}>
+        <form onSubmit={handleSubmit} className={commonModalClasses + " w-72"}>
           <Title>SignUp 🐛</Title>
-          <Input name="name" label="Name" placeholder="John Wick" />
-          <Input name="email" label="Email" placeholder="johnwick@gmail.com" />
-          <Input name="password" label="Password" placeholder="********" />
+          <Input
+            onChange={handleChange}
+            value={name}
+            name="name"
+            label="Name"
+            placeholder="John Wick"
+          />
+          <Input
+            onChange={handleChange}
+            value={email}
+            name="email"
+            label="Email"
+            placeholder="johnwick@gmail.com"
+          />
+          <Input
+            onChange={handleChange}
+            value={password}
+            name="password"
+            label="Password"
+            type="password"
+            placeholder="********"
+          />
           <Submit value="Sign Up" />
           <div className="flex justify-between">
             <CustomLink to="/forget-password">Forget Password?</CustomLink>
