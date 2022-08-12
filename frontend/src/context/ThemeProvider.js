0a9a163cb5c9fcc9fee17dd2a2e94fafbf5ba5b0
Mahendra <mahendrabrowny@gmail.com>
2022-08-12 14:08:08 +0530

    theme provider for toggling the theme

---
+++ frontend/src/context/ThemeProvider.js
@@ -0,0 +1,39 @@
+import React, { createContext } from "react";
+import { useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+const defaultTheme = "light";
+const darkTheme = "dark";
+
+const ThemeProvider = ({ children }) => {
+  const toggleTheme = () => {
+    const prevTheme = getTheme();
+    const newTheme = prevTheme === defaultTheme ? darkTheme : defaultTheme;
+
+    updateTheme(newTheme, prevTheme);
+  };
+
+  useEffect(() => {
+    const theme = getTheme();
+    if (!theme) updateTheme(defaultTheme);
+    else updateTheme(theme);
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const getTheme = () => localStorage.getItem("theme");
+
+const updateTheme = (theme, themeToRemove) => {
+  if (themeToRemove) document.documentElement.classList.remove(themeToRemove);
+
+  document.documentElement.classList.add(theme);
+  localStorage.setItem("theme", theme);
+};
+
+export default ThemeProvider;
