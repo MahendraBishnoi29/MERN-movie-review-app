3720cd2fb55637fcb32e24bf5802558f310bc682
Mahendra <mahendrabrowny@gmail.com>
2022-08-14 00:29:01 +0530

    custom toast component for notifications

--- frontend/src/context/NotificationProvider.js
+++ frontend/src/context/NotificationProvider.js
@@ -1,20 +1,48 @@
 import React, { createContext } from "react";
+import { useState } from "react";
 
-const NotificationContext = createContext();
+export const NotificationContext = createContext();
+
+let timeOutId;
 
 const NotificationProvider = ({ children }) => {
-  const updateNotification = () => {};
+  const [notification, setNotification] = useState("");
+  const [classes, setClasses] = useState("");
+
+  const updateNotification = (type, value) => {
+    if (timeOutId) clearTimeout(timeOutId);
+
+    switch (type) {
+      case "error":
+        setClasses("bg-red-500");
+        break;
+      case "success":
+        setClasses("bg-green-500");
+        break;
+      case "warning":
+        setClasses("bg-orange-500");
+        break;
+      default:
+        setClasses("bg-red-500");
+    }
+    setNotification(value);
+    timeOutId = setTimeout(() => {
+      setNotification("");
+    }, 3000);
+  };
 
   return (
     <NotificationContext.Provider value={{ updateNotification }}>
       {children}
-      <div className="fixed left-1/2 -translate-x-1/2 top-24 ">
-        <div className="shadow-md shadow-gray-400 bg-red-500 rounded gelatine">
-          <p className="text-white px-4 py-2 font-semibold">
-            Something went wrong..
-          </p>
+      {notification && (
+        <div className="fixed left-1/2 -translate-x-1/2 top-16 ">
+          <div className="shadow-md shadow-gray-400 rounded gelatine">
+            <p className={classes + " text-white px-4 py-2 font-semibold"}>
+              {notification}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </NotificationContext.Provider>
   );
 };
