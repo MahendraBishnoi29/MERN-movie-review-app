80d486239d2f7233b443db24b373e3437a239dd4
Mahendra <mahendrabrowny@gmail.com>
2022-08-14 00:28:36 +0530

    useNotification context

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,4 +1,7 @@
 import { useContext } from "react";
+import { NotificationContext } from "../context/NotificationProvider";
 import { ThemeContext } from "../context/ThemeProvider";
 
 export const useTheme = () => useContext(ThemeContext);
+
+export const useNotification = () => useContext(NotificationContext);
