3cb047fb34a84fb9476c0cc09c638c54df37ec3c
Mahendra <mahendrabrowny@gmail.com>
2022-08-13 22:38:40 +0530

    user email verify method done

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -6,6 +6,21 @@ export const createUser = async (userInfo) => {
     return data;
   } catch (error) {
     const { res } = error;
+    console.log(error.message);
+    if (res?.data) return res.data;
+
+    return error.message || error;
+  }
+};
+
+// Verify User
+export const verifyUserEmail = async (userInfo) => {
+  try {
+    const { data } = await client.post("/user/verify-email", userInfo);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    const { res } = error;
     if (res?.data) return res.data;
 
     return error.message || error;
