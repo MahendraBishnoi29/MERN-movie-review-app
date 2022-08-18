e83f3b3e53401904f58cc148cab65b41ef0e1ead
Mahendra <mahendrabrowny@gmail.com>
2022-08-18 20:44:00 +0530

    better naming in validation

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -3,14 +3,14 @@ const { check, validationResult } = require("express-validator");
 // Validate Complete User With All Fields
 exports.SignUpValidator = [
   check("name").trim().not().isEmpty().withMessage("Name is missing!"),
-  check("email").normalizeEmail().isEmail().withMessage("Email is Invalid!!"),
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
   check("password")
     .trim()
     .not()
     .isEmpty()
-    .withMessage("Please Enter Your Password!")
-    .isLength({ min: 6, max: 15 })
-    .withMessage("Password must be 6 to 15 characters long!"),
+    .withMessage("Password is missing!")
+    .isLength({ min: 6, max: 10 })
+    .withMessage("Password must be 6 to 10 characters long!"),
 ];
 
 // Validate Password Only
@@ -25,8 +25,8 @@ exports.validatePassword = [
 ];
 
 exports.SignInValidator = [
-  check("email").normalizeEmail().isEmail().withMessage("Wrong Email!"),
-  check("password").trim().not().isEmpty().withMessage("Wrong Password!"),
+  check("email").normalizeEmail().isEmail().withMessage("Wrong/Invalid Email!"),
+  check("password").trim().not().isEmpty().withMessage("Password is missing!"),
 ];
 
 // For formating Error Message
