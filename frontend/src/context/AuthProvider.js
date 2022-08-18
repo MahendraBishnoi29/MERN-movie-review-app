a9353a8cbdd2476b609f13478a90aa655c11c696
Mahendra <mahendrabrowny@gmail.com>
2022-08-18 23:13:04 +0530

    jwt token auth

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from "react";
-import { SignInUser } from "../api/auth";
+import { useEffect } from "react";
+import { getIsAuth, SignInUser } from "../api/auth";
 
 export const AuthContext = createContext();
 
@@ -28,13 +29,32 @@ const AuthProvider = ({ children }) => {
       error: "",
     });
 
-    console.log(setAuthInfo);
-
     localStorage.setItem("auth-token", user.token);
   };
 
+  const isAuth = async () => {
+    const token = localStorage.getItem("auth-token");
+    if (!token) return;
+
+    setAuthInfo({ ...authInfo, isPending: true });
+
+    const { error, user } = await getIsAuth(token);
+
+    if (error) return setAuthInfo({ ...authInfo, isPending: false, error });
+    setAuthInfo({
+      profile: { ...user },
+      isLoggedIn: true,
+      isPending: false,
+      error: "",
+    });
+  };
+
+  useEffect(() => {
+    isAuth();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ authInfo, handleLogIn }}>
+    <AuthContext.Provider value={{ authInfo, handleLogIn, isAuth }}>
       {children}
     </AuthContext.Provider>
   );
