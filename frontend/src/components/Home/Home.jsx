0fb0f4f20523a9a8ddb585599eef780afaedd86a
Mahendra <mahendrabrowny@gmail.com>
2022-08-19 20:13:47 +0530

    home page basic setup

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useAuth } from "../../hooks";
+import Container from "../Navbar/Container";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { authInfo } = useAuth();
+  console.log(authInfo);
+
+  return (
+    <Container>
+      <p className="text-2xl text-center bg-blue-50 p-3">
+        Looks Like You Haven't Verified Your Account ,&nbsp;
+        <button className="text-blue-500 font-semibold hover:underline">
+          Click here to Verify :)
+        </button>
+      </p>
+    </Container>
+  );
 };
 
 export default Home;
