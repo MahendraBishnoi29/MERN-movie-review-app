08b144ce5c7cc8736cd7dc3ff57442c8ef61fcb1
Mahendra <mahendrabrowny@gmail.com>
2022-08-19 18:30:39 +0530

    confirm password verification done

--- frontend/src/components/Auth/ConfirmPassword.jsx
+++ frontend/src/components/Auth/ConfirmPassword.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { ImSpinner3 } from "react-icons/im";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { verifyPasswordResetToken } from "../../api/auth";
 import { commonModalClasses } from "../../utils/theme";
 import FormContainer from "../Form/formContainer/FormContainer";
 import Input from "../Form/Input";
@@ -7,6 +13,62 @@ import Title from "../Form/Title";
 import Container from "../Navbar/Container";
 
 const ConfirmPassword = () => {
+  // http://localhost:3000/reset-password?token=a24d829a36c1de6d961012fa8cd99aa096cce9c500b6a346085950b5e5f1&id=62ff7398daa1e2162d492434
+
+  const [verifying, setVerifying] = useState(true);
+  const [valid, setValid] = useState(false);
+
+  const navigate = useNavigate();
+
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
+  const id = searchParams.get("id");
+
+  useEffect(() => {
+    isValidToken();
+  }, []);
+
+  const isValidToken = async () => {
+    const { error, valid } = await verifyPasswordResetToken(token, id);
+    setVerifying(false);
+    if (error) {
+      toast.error(error);
+      return navigate("/reset-password", { replace: true });
+    }
+
+    if (!valid) {
+      setValid(false);
+      setVerifying(false);
+    }
+
+    setValid(true);
+  };
+
+  if (verifying)
+    return (
+      <FormContainer>
+        <Container>
+          <div className="flex space-x-2 items-center">
+            <h2 className="text-3xl font-semibold dark:text-white text-primary">
+              Verifying Your Token
+            </h2>
+            <ImSpinner3 className="animate-spin text-3xl dark:text-white text-primary" />
+          </div>
+        </Container>
+      </FormContainer>
+    );
+
+  if (!valid)
+    return (
+      <FormContainer>
+        <Container>
+          <h2 className="text-3xl font-semibold dark:text-white text-primary">
+            Sorry the Token Is Invalid!
+          </h2>
+        </Container>
+      </FormContainer>
+    );
+
   return (
     <FormContainer>
       <Container>
