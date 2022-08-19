05fa7f2c4f2e23cfafcbee88b9eee8c63859b415
Mahendra <mahendrabrowny@gmail.com>
2022-08-19 22:10:36 +0530

    different ui components for auth users

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks";
 import Container from "../Navbar/Container";
 
 const Home = () => {
   const { authInfo } = useAuth();
-  console.log(authInfo);
+  const navigate = useNavigate();
+
+  const { isLoggedIn } = authInfo;
+  const isVerified = authInfo.profile?.isVerified;
+
+  const navigateToVerification = () => {
+    navigate("/email-verification", { state: { user: authInfo.profile } });
+  };
 
   return (
     <Container>
-      <p className="text-2xl text-center bg-blue-50 p-3">
-        Looks Like You Haven't Verified Your Account ,&nbsp;
-        <button className="text-blue-500 font-semibold hover:underline">
-          Click here to Verify :)
-        </button>
-      </p>
+      {isLoggedIn && !isVerified ? (
+        <p className="text-2xl text-center bg-blue-50 p-3">
+          Looks Like You Haven't Verified Your Account ,&nbsp;
+          <button
+            onClick={navigateToVerification}
+            className="text-blue-500 font-semibold hover:underline"
+          >
+            Click here to Verify :)
+          </button>
+        </p>
+      ) : null}
     </Container>
   );
 };
