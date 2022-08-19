5ba2af5d12456f49e28df7ca7fe8b64195e59800
Mahendra <mahendrabrowny@gmail.com>
2022-08-19 22:42:06 +0530

    new component for refactoring home page

---
+++ frontend/src/components/Home/IsVerified.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks";
+import Container from "../Navbar/Container";
+
+const IsVerified = () => {
+  const { authInfo } = useAuth();
+  const navigate = useNavigate();
+
+  const { isLoggedIn } = authInfo;
+  const isVerified = authInfo.profile?.isVerified;
+
+  const navigateToVerification = () => {
+    navigate("/email-verification", { state: { user: authInfo.profile } });
+  };
+
+  return (
+    <Container>
+      {isLoggedIn && !isVerified ? (
+        <p className="text-2xl text-center bg-blue-50 p-3">
+          Looks Like You Haven't Verified Your Account ,&nbsp;
+          <button
+            onClick={navigateToVerification}
+            className="text-blue-500 font-semibold hover:underline"
+          >
+            Click here to Verify :)
+          </button>
+        </p>
+      ) : null}
+    </Container>
+  );
+};
+
+export default IsVerified;
