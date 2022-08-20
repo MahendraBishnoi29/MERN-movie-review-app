4f0c9bbd13436c286eecefdf156c95aad4bb5268
Mahendra <mahendrabrowny@gmail.com>
2022-08-20 16:19:54 +0530

    app.use actor router

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 require("./db");
 const userRouter = require("./routes/user");
+const actorRouter = require("./routes/actor");
 const { handleNotFound } = require("./utils/helper");
 
 const app = express();
@@ -10,18 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/user", userRouter);
+app.use("/api/actor", actorRouter);
 
 app.use("/*", handleNotFound);
-
-// app.post("/login", (req, res, next) => {
-//   const { email, password } = req.body;
-//   if (!email || !password) {
-//     return res
-//       .status(400)
-//       .json({ message: "No User Found Please SignUp first" });
-//   }
-//   next();
-// });
 
 app.listen(8000, () => {
   console.log("Server is Running");
