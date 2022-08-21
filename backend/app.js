1fb455e1a9171de6cd0eb5bd2aeda3d97cf39f64
Mahendra <mahendrabrowny@gmail.com>
2022-08-21 18:28:30 +0530

    listening to movie route in main app

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 require("./db");
+// Routes
 const userRouter = require("./routes/user");
 const actorRouter = require("./routes/actor");
+const movieRouter = require("./routes/movie");
+// Helpers
 const { handleNotFound } = require("./utils/helper");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 app.use("/api/user", userRouter);
 app.use("/api/actor", actorRouter);
+app.use("/api/movie", movieRouter);
 
 app.use("/*", handleNotFound);
 
