6ea2eee7ad73887bfd83d3162d1cd75f45d59235
Mahendra <mahendrabrowny@gmail.com>
2022-08-21 18:47:44 +0530

    destructuring url & public id

--- backend/controllers/movie.js
+++ backend/controllers/movie.js
@@ -1,15 +1,19 @@
 const cloudinary = require("../utils/cloud");
 
+// UPLOAD TRAILER FUNCTION
 const uploadTrailer = async (req, res) => {
   const { file } = req;
 
   if (!file) return res.json({ error: "Video file is missing!" });
 
-  const trailer = await cloudinary.v2.uploader.upload(file?.path, {
-    resource_type: "video",
-  });
+  const { secure_url: url, public_id } = await cloudinary.v2.uploader.upload(
+    file?.path,
+    {
+      resource_type: "video",
+    }
+  );
 
-  res.json(trailer);
+  res.status(201).json({ url, public_id });
 };
 
 module.exports = { uploadTrailer };
