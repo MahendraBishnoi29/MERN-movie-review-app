abf42c8eb6bfb27d0b1cfaf7d9be0b0b0746da26
Mahendra <mahendrabrowny@gmail.com>
2022-08-21 15:50:32 +0530

    actor routes protected for admins only

--- backend/routes/actor.js
+++ backend/routes/actor.js
@@ -8,11 +8,14 @@ const {
   getLatestActor,
   getSingleActor,
 } = require("../controllers/actor");
+const { isAdmin, IsAuth } = require("../middlewares/authMiddlware");
 const { uploadImage } = require("../middlewares/multer");
 const { actorInfoValidator, validate } = require("../middlewares/validator");
 
 router.post(
   "/create",
+  IsAuth,
+  isAdmin,
   uploadImage.single("avatar"),
   actorInfoValidator,
   validate,
@@ -21,15 +24,17 @@ router.post(
 
 router.post(
   "/update/:actorId",
+  IsAuth,
+  isAdmin,
   uploadImage.single("avatar"),
   actorInfoValidator,
   validate,
   updateActor
 );
 
-router.delete("/delete/:actorId", deleteActor);
-router.get("/search", searchActor);
-router.get("/latest-uploads", getLatestActor);
+router.delete("/delete/:actorId", IsAuth, isAdmin, deleteActor);
+router.get("/search", IsAuth, isAdmin, searchActor);
+router.get("/latest-uploads", IsAuth, isAdmin, getLatestActor);
 router.get("/single/:id", getSingleActor);
 
 module.exports = router;
