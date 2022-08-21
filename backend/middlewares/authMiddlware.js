26dce68a87eb59817790b96c929b12f6ab0d6b43
Mahendra <mahendrabrowny@gmail.com>
2022-08-21 15:49:49 +0530

    fix: typo in IsAuth middleware

--- backend/middlewares/authMiddlware.js
+++ backend/middlewares/authMiddlware.js
@@ -3,7 +3,9 @@ const jwt = require("jsonwebtoken");
 
 exports.IsAuth = async (req, res, next) => {
   const token = req.headers?.authorization;
-  const jwtToken = token.split("Bearer ")[1];
+  if (!token) return res.json({ error: "Invalid Token" });
+
+  const jwtToken = token?.split("Bearer ")[1];
 
   if (!jwtToken) return res.json({ error: "invalid Token" });
 
@@ -22,7 +24,7 @@ exports.isAdmin = (req, res, next) => {
   const { user } = req;
 
   if (user.role !== "admin")
-    return res.json("Unauthorized Access, only Admins can Access!");
+    return res.json("Unauthorized Access, only Admins can Access this Route!");
 
   next();
 };
