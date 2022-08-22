0957608d4bf6494badf064c6ede9668df7992a23
Mahendra <mahendrabrowny@gmail.com>
2022-08-22 22:59:52 +0530

    CSS 😭😭😭

--- frontend/src/components/Admin/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard.jsx
@@ -3,7 +3,7 @@ import { AiOutlinePlus } from "react-icons/ai";
 
 const Dashboard = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between relative">
       <input
         type="text"
         placeholder="search movies"
@@ -11,10 +11,19 @@ const Dashboard = () => {
         dark:text-white focus:border-primary"
       />
 
-      <button className="flex items-center space-x-1 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1">
+      <button className="flex items-center space-x-1 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg mr-8 px-3 py-1">
         <span>Create</span>
         <AiOutlinePlus />
       </button>
+
+      <div className="absolute right-0 mr-4 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded">
+        <button className="dark:text-white text-secondary transition hover:opacity-80">
+          Add Movie
+        </button>
+        <button className="dark:text-white text-secondary transition hover:opacity-80">
+          Add Actor
+        </button>
+      </div>
     </div>
   );
 };
