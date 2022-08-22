3fd0dd13a40381519b5b3b6386c7b430de558811
Mahendra <mahendrabrowny@gmail.com>
2022-08-22 18:43:45 +0530

    send user's role to client side

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -121,6 +121,7 @@ const verifyEmail = async (req, res) => {
       email: user.email,
       token: jwtToken,
       isVerified: user.isVerified,
+      role: user.role,
     },
     message: "Your Email is Verified ✔",
   });
