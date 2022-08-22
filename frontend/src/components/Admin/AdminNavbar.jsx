c1662357cd2317c00876fd7c542ddf14d92f096c
Mahendra <mahendrabrowny@gmail.com>
2022-08-22 22:17:53 +0530

    sepereate component for admin Navbar

---
+++ frontend/src/components/Admin/AdminNavbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiMoviePlay } from "react-icons/bi";
+import { FaUserNinja } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
+
+const AdminNavbar = () => {
+  return (
+    <nav className="flex flex-col justify-between w-48 min-h-screen bg-secondary border-r border-gray-300">
+      <ul className="pl-4">
+        <li className="mb-8">
+          <Link to="/">
+            <img src="./logo2.png" alt="logo" className="h-14 p-2" />
+          </Link>
+        </li>
+
+        <li>
+          <NavItem to="/">
+            <AiOutlineHome />
+            <span>Home</span>
+          </NavItem>
+        </li>
+        <li>
+          <NavItem to="/movies">
+            <BiMoviePlay /> <span>Movies</span>
+          </NavItem>
+        </li>
+        <li>
+          <NavItem to="/actors">
+            <FaUserNinja /> <span>Actors</span>
+          </NavItem>
+        </li>
+      </ul>
+
+      <div className="flex flex-col items-start p-8">
+        <span className="font-semibold text-white text-xl">Admin</span>
+        <button className="flex items-center text-dark-subtle text-sm hover:text-white transition space-x-1">
+          <FiLogOut />
+          <span> Log Out</span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+const NavItem = ({ children, to }) => {
+  const commonClasses =
+    " flex items-center text-lg space-x-2 p-2 hover:opacity-80";
+
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        (isActive ? "text-white" : "text-gray-400") + commonClasses
+      }
+      to={to}
+    >
+      {children}
+    </NavLink>
+  );
+};
+
+export default AdminNavbar;
