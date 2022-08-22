ea01a9d98b9eac690fa7d845adc7aeefff7d0f17
Mahendra <mahendrabrowny@gmail.com>
2022-08-22 22:17:01 +0530

    refactoring

--- frontend/src/utils/AdminNavigator/AdminNavigator.jsx
+++ frontend/src/utils/AdminNavigator/AdminNavigator.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Actors from "../../components/Admin/Actors";
+import AdminNavbar from "../../components/Admin/AdminNavbar";
 import Dashboard from "../../components/Admin/Dashboard";
 import Movies from "../../components/Admin/Movies";
 import NotFound from "../../components/Home/NotFound";
 
 const AdminNavigator = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/actors" element={<Actors />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
+    <div className="flex">
+      <AdminNavbar />
+      <div className="flex-1 p-2 max-w-screen-xl">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
