4d4971bd4003702e0ec6197c80109b07a20ed0e4
Mahendra <mahendrabrowny@gmail.com>
2022-08-24 12:16:06 +0530

    form refactor & ui

--- frontend/src/components/Admin/Movie/MovieForm.jsx
+++ frontend/src/components/Admin/Movie/MovieForm.jsx
@@ -1,23 +1,43 @@
 import React from "react";
 
+const commonInputClasses =
+  "w-full bg-transparent dark:text-white text-primary outline-none  dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition";
+
 const MovieForm = () => {
   return (
     <form className="flex space-x-3">
-      <div className="w-[70%] h-5 ">
-        <label
-          htmlFor="title"
-          className="dark:text-dark-subtle text-light-subtle font-semibold"
-        >
-          Title
-        </label>
-        <input
-          type="text"
-          placeholder="John Wick"
-          className="w-full bg-transparent dark:text-white text-primary outline-none border-b-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition font-semibold"
-        />
+      <div className="w-[70%] h-5 space-y-5">
+        <div>
+          <Label htmlFor="title">Title</Label>
+          <input
+            type="text"
+            placeholder="John Wick"
+            className={commonInputClasses + " border-b-2 font-semibold"}
+          />
+        </div>
+
+        <div>
+          <Label htmlFor="storyLine">Story Line</Label>
+          <textarea
+            id="stroyLine"
+            className={commonInputClasses + " resize-none h-24 border-b-2"}
+            placeholder="Movie story line..."
+          ></textarea>
+        </div>
       </div>
       <div className="w-[30%] h-5 "></div>
     </form>
+  );
+};
+
+const Label = ({ children, htmlFor }) => {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className="dark:text-dark-subtle text-light-subtle font-semibold"
+    >
+      {children}
+    </label>
   );
 };
 
