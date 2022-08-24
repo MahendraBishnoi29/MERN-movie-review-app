02d2343cbd3f0fa12d6fdfae1968c38408c67ec4
Mahendra <mahendrabrowny@gmail.com>
2022-08-24 17:36:42 +0530

    refactor

--- frontend/src/components/Admin/Movie/MovieForm.jsx
+++ frontend/src/components/Admin/Movie/MovieForm.jsx
@@ -3,9 +3,69 @@ import { commonInputClasses } from "../../../utils/theme";
 import TagsInput from "../../Form/tagsInput/TagsInput";
 import LiveSearch from "../../LiveSearch/LiveSearch";
 
+export const results = [
+  {
+    id: "1",
+    avatar:
+      "https://images.unsplash.com/photo-1643713303351-01f540054fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "John Doe",
+  },
+  {
+    id: "2",
+    avatar:
+      "https://images.unsplash.com/photo-1643883135036-98ec2d9e50a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Chandri Anggara",
+  },
+  {
+    id: "3",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "4",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+  {
+    id: "5",
+    avatar:
+      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Amin RK",
+  },
+  {
+    id: "6",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+  {
+    id: "7",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+  {
+    id: "8",
+    avatar:
+      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
+    name: "Edward Howell",
+  },
+];
+
 const MovieForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const renderItem = (res) => {
+    return (
+      <div key={res.id} className="flex space-x-2 rounded overflow-hidden">
+        <img src={res.avatar} alt="" className="w-14 h-14 object-cover" />
+        <p className="dark:text-white font-semibold">{res.name}</p>
+      </div>
+    );
   };
 
   return (
@@ -34,7 +94,12 @@ const MovieForm = () => {
           <TagsInput name="tags" />
         </div>
 
-        <LiveSearch />
+        <LiveSearch
+          placeholder="Search profile"
+          results={results}
+          renderItem={renderItem}
+          onSelect={(res) => console.log(res)}
+        />
       </div>
       <div className="w-[30%] h-5 "></div>
     </form>
