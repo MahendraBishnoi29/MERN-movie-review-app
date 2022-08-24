be97fad357cd42dacaa9bdbde5535641f2984658
Mahendra <mahendrabrowny@gmail.com>
2022-08-24 15:52:32 +0530

    only show results when input is in focus state

--- frontend/src/components/LiveSearch/LiveSearch.jsx
+++ frontend/src/components/LiveSearch/LiveSearch.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { commonInputClasses } from "../../utils/theme";
 
 export const results = [
@@ -41,30 +42,50 @@ export const results = [
 ];
 
 const LiveSearch = () => {
+  const [displaySearch, setDisplaySearch] = useState(false);
+
+  const handleOnFocus = () => {
+    if (results.length) setDisplaySearch(true);
+  };
+
+  const handleOnBlur = () => {
+    setDisplaySearch(false);
+  };
+
   return (
     <div className="relative">
       <input
+        onBlur={handleOnBlur}
+        onFocus={handleOnFocus}
         type="text"
         placeholder="search profile"
         className={commonInputClasses + " rounded border-2 p-1 text-lg"}
       />
-      <div className="absolute right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1 overflow-auto custom-scrollbar">
-        {results.map(({ id, name, avatar }) => {
-          return (
-            <div
-              key={id}
-              className="cursor-pointer rounded overflow-hidden dark:hover:bg-dark-subtle hover:bg-light-subtle transition flex space-x-2"
-            >
-              <img
-                src={avatar}
-                alt="actorImg"
-                className="w-16 h-16 rounded object-cover"
-              />
-              <p className="dark:text-white font-semibold">{name}</p>
-            </div>
-          );
-        })}
-      </div>
+      <SearchResults results={results} visible={displaySearch} />
+    </div>
+  );
+};
+
+const SearchResults = ({ visible, results }) => {
+  if (!visible) return null;
+
+  return (
+    <div className="absolute right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1 overflow-auto custom-scrollbar">
+      {results.map(({ id, name, avatar }) => {
+        return (
+          <div
+            key={id}
+            className="cursor-pointer rounded overflow-hidden dark:hover:bg-dark-subtle hover:bg-light-subtle transition flex space-x-2"
+          >
+            <img
+              src={avatar}
+              alt="actorImg"
+              className="w-12 h-12 rounded object-cover"
+            />
+            <p className="dark:text-white font-semibold">{name}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
