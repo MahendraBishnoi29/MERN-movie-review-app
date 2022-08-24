2c6e92d5fb2a9c46b0aa4f4e03283c599956a8c8
Mahendra <mahendrabrowny@gmail.com>
2022-08-24 14:47:48 +0530

    common input classes

--- frontend/src/utils/theme.js
+++ frontend/src/utils/theme.js
@@ -1,2 +1,5 @@
 export const commonModalClasses =
   "dark:bg-secondary bg-white drop-shadow-lg rounded space-y-4 p-6 ";
+
+export const commonInputClasses =
+  "w-full bg-transparent dark:text-white text-primary outline-none  dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition";
