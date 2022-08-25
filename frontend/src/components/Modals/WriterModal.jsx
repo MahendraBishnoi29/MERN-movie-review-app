08dd4b34485e22c45832ed4323003f97b510dadf
Mahendra <mahendrabrowny@gmail.com>
2022-08-25 13:13:09 +0530

    selected actors ui fixed

--- frontend/src/components/Modals/WriterModal.jsx
+++ frontend/src/components/Modals/WriterModal.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import ModalContainer from "./ModalContainer";
 
 const WriterModal = ({ profiles = [], visible, onClose }) => {
   return (
     <ModalContainer onClose={onClose} visible={visible}>
-      {profiles.map(({ id, name, avatar }) => {
-        return (
-          <div key={id} className="flex">
-            <img src={avatar} alt={name} className="" />
-            <p className=""> {name} </p>
-          </div>
-        );
-      })}
+      <div className="space-y-2">
+        {profiles.map(({ id, name, avatar }) => {
+          return (
+            <div key={id} className="flex space-x-3">
+              <img
+                src={avatar}
+                alt={name}
+                className="w-16 h-16 rounded object-cover"
+              />
+              <p className="w-full font-semibold dark:text-white text-primary">
+                {name}
+              </p>
+              <button className="dark:text-white text-primary hover:opacity-80 transition p-2">
+                <AiOutlineClose />
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </ModalContainer>
   );
 };
