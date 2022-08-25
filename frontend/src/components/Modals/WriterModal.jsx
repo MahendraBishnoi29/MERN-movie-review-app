61cdc1f358f5ed3386374664250cef11a0eaf1a1
Mahendra <mahendrabrowny@gmail.com>
2022-08-25 14:34:41 +0530

    remove writers from modal on click

--- frontend/src/components/Modals/WriterModal.jsx
+++ frontend/src/components/Modals/WriterModal.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import ModalContainer from "./ModalContainer";
 
-const WriterModal = ({ profiles = [], visible, onClose }) => {
+const WriterModal = ({ profiles = [], visible, onClose, onRemoveProfile }) => {
   return (
-    <ModalContainer onClose={onClose} visible={visible}>
-      <div className="space-y-2">
+    <ModalContainer ignoreContainer onClose={onClose} visible={visible}>
+      <div className="space-y-2 dark:bg-primary bg-white rounded max-w-[40rem] max-h-[34rem] overflow-auto p-2 custom-scrollbar">
         {profiles.map(({ id, name, avatar }) => {
           return (
             <div key={id} className="flex space-x-3">
               <img
                 src={avatar}
                 alt={name}
-                className="w-16 h-16 rounded object-cover"
+                className="w-16 h-16 rounded object-cover aspect-square"
               />
               <p className="w-full font-semibold dark:text-white text-primary">
                 {name}
               </p>
-              <button className="dark:text-white text-primary hover:opacity-80 transition p-2">
+              <button
+                onClick={() => onRemoveProfile(id)}
+                className="dark:text-white text-primary hover:opacity-80 transition p-2"
+              >
                 <AiOutlineClose />
               </button>
             </div>
