939c87c305045dc32e2190d38d9a94b4d8dc6116
Mahendra <mahendrabrowny@gmail.com>
2022-08-25 22:24:13 +0530

    fixed form submit bug

--- frontend/src/components/Form/Submit.jsx
+++ frontend/src/components/Form/Submit.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { ImSpinner3 } from "react-icons/im";
 
-const Submit = ({ value, busy }) => {
+const Submit = ({ value, busy, type, onClick }) => {
   return (
     <button
-      type="submit"
+      type={type || "submit"}
+      onClick={onClick}
       className="cursor-pointer w-full bg-secondary dark:text-secondary rounded dark:bg-white text-white hover:bg-opacity-80 transition font-semibold text-lg h-10 flex justify-center items-center"
     >
       {busy ? <ImSpinner3 className="animate-spin" /> : value}
