c8495c4a94860cb03c0e9899d6fd7e4f40f093b0
Mahendra <mahendrabrowny@gmail.com>
2022-08-25 12:42:50 +0530

    custom badge for showing all actors from dropdown

--- frontend/src/components/Admin/Movie/MovieForm.jsx
+++ frontend/src/components/Admin/Movie/MovieForm.jsx
@@ -99,7 +99,7 @@ const MovieForm = () => {
     setMovieInfo({ ...movieInfo, tags });
   };
 
-  const { title, storyLine, director } = movieInfo;
+  const { title, storyLine, director, writers } = movieInfo;
 
   const updateDirector = (profile) => {
     setMovieInfo({ ...movieInfo, director: profile });
@@ -161,7 +161,15 @@ const MovieForm = () => {
         </div>
 
         <div>
-          <Label htmlFor="writers">Writers</Label>
+          <div className="flex justify-between">
+            <LabelWithBadge badge={writers.length} htmlFor="writers">
+              Writers
+            </LabelWithBadge>
+            <button className="dark:text-white text-primary hover:underline transition">
+              View All
+            </button>
+          </div>
+
           <LiveSearch
             name="writers"
             onSelect={updateWriters}
@@ -186,6 +194,23 @@ const Label = ({ children, htmlFor }) => {
     >
       {children}
     </label>
+  );
+};
+
+const LabelWithBadge = ({ children, htmlFor, badge }) => {
+  const renderBadge = () => {
+    return (
+      <span className="dark:bg-dark-subtle bg-light-subtle text-white absolute top-0 right-0 w-5 h-5 rounded-full flex justify-center items-center translate-x-2 -translate-y-1 text-sm">
+        {badge <= 9 ? badge : "9+"}
+      </span>
+    );
+  };
+
+  return (
+    <div className="relative">
+      <Label htmlFor={htmlFor}> {children} </Label>
+      {renderBadge()}
+    </div>
   );
 };
 
