4d265792d4edc58ec7cee464173de451f6f82abb
Mahendra <mahendrabrowny@gmail.com>
2022-08-25 14:35:03 +0530

    refactor children render method

--- frontend/src/components/Modals/ModalContainer.jsx
+++ frontend/src/components/Modals/ModalContainer.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-const ModalContainer = ({ visible, children, onClose }) => {
+const ModalContainer = ({ visible, children, onClose, ignoreContainer }) => {
   if (!visible) return null;
 
   const handleClick = (e) => {
     if (e.target.id === "modal-container") onClose();
+  };
+
+  const renderChildren = () => {
+    if (ignoreContainer) return children;
+
+    return (
+      <div className="dark:bg-primary bg-white rounded w-[40rem] h-[34rem] overflow-auto p-2 custom-scrollbar">
+        {children}
+      </div>
+    );
   };
 
   return (
@@ -13,9 +23,7 @@ const ModalContainer = ({ visible, children, onClose }) => {
       id="modal-container"
       className="fixed inset-0 dark:bg-white bg-primary dark:bg-opacity-50 bg-opacity-50 backdrop-blur-sm flex items-center justify-center"
     >
-      <div className="dark:bg-primary bg-white rounded w-[40rem] h-[34rem] overflow-auto p-2 custom-scrollbar">
-        {children}
-      </div>
+      {renderChildren()}
     </div>
   );
 };
