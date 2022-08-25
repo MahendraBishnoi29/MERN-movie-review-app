641d17900ea4b1dde05fd491779ac2aba624b909
Mahendra <mahendrabrowny@gmail.com>
2022-08-25 12:55:17 +0530

    modal container for movie modal

---
+++ frontend/src/components/Modals/ModalContainer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ModalContainer = ({ visible, children, onClose }) => {
+  if (!visible) return null;
+
+  const handleClick = (e) => {
+    if (e.target.id === "modal-container") onClose();
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      id="modal-container"
+      className="fixed inset-0 dark:bg-white bg-primary dark:bg-opacity-50 bg-opacity-50 backdrop-blur-sm flex items-center justify-center"
+    >
+      <div className="dark:bg-primary bg-white rounded w-[40rem] h-[34rem] overflow-auto p-2 custom-scrollbar">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ModalContainer;
