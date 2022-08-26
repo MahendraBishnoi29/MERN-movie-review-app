a71276a6ed245cd1b71fc18e69f3b04544ecf741
Mahendra <mahendrabrowny@gmail.com>
2022-08-26 22:29:45 +0530

    creating create actor form modal

--- frontend/src/components/Modals/ActorUpload.jsx
+++ frontend/src/components/Modals/ActorUpload.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import ActorForm from "../Form/ActorForm";
 import ModalContainer from "./ModalContainer";
 
 const ActorUpload = ({ visible, onClose }) => {
   return (
-    <ModalContainer
-      visible={visible}
-      onClose={onClose}
-      ignoreContainer
-    ></ModalContainer>
+    <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
+      <ActorForm title="Create a new Actor" btnTitle="Create" />
+    </ModalContainer>
   );
 };
 
