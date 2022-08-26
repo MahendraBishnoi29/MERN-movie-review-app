5a9a650d1b5c04a7703d602f31ae58b1ffd8371a
Mahendra <mahendrabrowny@gmail.com>
2022-08-26 17:06:43 +0530

    mapping and rendering options

--- frontend/src/components/PosterSelector/Selector.jsx
+++ frontend/src/components/PosterSelector/Selector.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Selector = ({ name, value, onChange, label }) => {
+const Selector = ({ name, options, value, onChange, label }) => {
   return (
     <select
       id={name}
@@ -10,6 +10,13 @@ const Selector = ({ name, value, onChange, label }) => {
       className="pr-1 border-2 dark:border-dark-subtle border-light-subtle p-1 dark:focus:border-white focus:border-primary outline-none transition rounded bg-tranparent text-light-subtle dark:text-dark-subtle dark:focus:text-white focus:text-primary"
     >
       <option value="">{label}</option>
+      {options.map(({ title, value }) => {
+        return (
+          <option value={value} key={title}>
+            {title}
+          </option>
+        );
+      })}
     </select>
   );
 };
