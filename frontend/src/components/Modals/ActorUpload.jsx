ee0c85590186142b177dca872f637eac06f9be2f
Mahendra <mahendrabrowny@gmail.com>
2022-08-27 18:31:42 +0530

    submit create actor form

--- frontend/src/components/Modals/ActorUpload.jsx
+++ frontend/src/components/Modals/ActorUpload.jsx
@@ -3,9 +3,17 @@ import ActorForm from "../Form/ActorForm";
 import ModalContainer from "./ModalContainer";
 
 const ActorUpload = ({ visible, onClose }) => {
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
-      <ActorForm title="Create a new Actor" btnTitle="Create" />
+      <ActorForm
+        onSubmit={handleSubmit}
+        title="Create a new Actor"
+        btnTitle="Create"
+      />
     </ModalContainer>
   );
 };
