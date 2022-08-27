54a7d4e8fd96a736f170122d2cf118389988a5e6
Mahendra <mahendrabrowny@gmail.com>
2022-08-27 23:53:20 +0530

    debouncing function

--- frontend/src/components/Admin/Dashboard/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import MovieUpload from "../Upload/MovieUpload";
 
+let timeoutId;
+const debounce = (func, delay) => {
+  return (...args) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func().apply(null, ...args);
+    }, delay);
+  };
+};
+
 const Dashboard = () => {
-  return <MovieUpload />;
+  return (
+    <div className="p-14">
+      <input type="text" className="border border-gray-500" />
+    </div>
+  );
 };
 
 export default Dashboard;
