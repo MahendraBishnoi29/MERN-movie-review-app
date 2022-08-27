bfa60b1be5335a20b12612bc6de69c9854797fb6
Mahendra <mahendrabrowny@gmail.com>
2022-08-27 22:16:34 +0530

    handeling promises with react toastify

--- frontend/src/components/Modals/ActorUpload.jsx
+++ frontend/src/components/Modals/ActorUpload.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { toast } from "react-toastify";
 import { createActor } from "../../api/actor";
 import ActorForm from "../Form/ActorForm";
 import ModalContainer from "./ModalContainer";
 
 const ActorUpload = ({ visible, onClose }) => {
   const handleSubmit = async (data) => {
-    const res = await createActor(data);
+    const res = await toast.promise(createActor(data), {
+      pending: "Creating Actor...",
+      success: "Actor Created Successfully 🎉",
+      error: "error creating Actor!",
+    });
+
     console.log(res);
   };
 
