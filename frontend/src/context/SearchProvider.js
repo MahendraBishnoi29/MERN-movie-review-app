94df5bcc026a4beac15fa36faf3e3f8c14ffc129
Mahendra <mahendrabrowny@gmail.com>
2022-08-28 23:11:44 +0530

    search provider

--- frontend/src/context/SearchProvider.js
+++ frontend/src/context/SearchProvider.js
@@ -1,4 +1,6 @@
 import React, { createContext } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const SearchContext = createContext();
 
@@ -13,7 +15,37 @@ const debounce = (func, delay) => {
 };
 
 const SearchProvider = ({ children }) => {
-  return <SearchContext.Provider> {children} </SearchContext.Provider>;
+  const [searching, setSearching] = useState(false);
+  const [results, setResults] = useState([]);
+  const [resultNotFound, setResultNotFound] = useState(false);
+
+  const search = async (method, query) => {
+    const { error, result } = await method(query);
+    if (error) return toast.error(error);
+    if (!result.length) return setResultNotFound(true);
+
+    setResults(results);
+  };
+
+  const debounceFunc = debounce(search, 300);
+
+  const handleSearch = (method, query) => {
+    setSearching(true);
+    if (!query.trim()) {
+      setSearching(false);
+      setResults([]);
+      setResultNotFound(false);
+    }
+    debounceFunc(method, query);
+  };
+
+  return (
+    <SearchContext.Provider
+      value={{ handleSearch, searching, resultNotFound, results }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
 };
 
 export default SearchProvider;
