16827cb659477aaeb6a1bf6a2b370f46053b01a2
Mahendra <mahendrabrowny@gmail.com>
2022-08-28 22:56:30 +0530

    live search function setup

--- frontend/src/components/Admin/Dashboard/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard/Dashboard.jsx
@@ -6,15 +6,29 @@ const debounce = (func, delay) => {
   return (...args) => {
     if (timeoutId) clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
-      func().apply(null, ...args);
+      func.apply(null, args);
     }, delay);
   };
 };
 
 const Dashboard = () => {
+  const search = (value) => {
+    console.log(value);
+  };
+
+  const debounceSeach = debounce(search, 500);
+
+  const handleChange = ({ target }) => {
+    debounceSeach(target.value);
+  };
+
   return (
     <div className="p-14">
-      <input type="text" className="border border-gray-500" />
+      <input
+        onChange={handleChange}
+        type="text"
+        className="border border-gray-500"
+      />
     </div>
   );
 };
