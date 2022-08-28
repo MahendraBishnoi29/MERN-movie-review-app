34aeb24df18bf2abd118edd94c7d50d28a534520
Mahendra <mahendrabrowny@gmail.com>
2022-08-28 22:58:34 +0530

    search context for live search

---
+++ frontend/src/context/SearchProvider.js
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+const SearchContext = createContext();
+
+const SearchProvider = () => {
+  return <div>SearchProvider</div>;
+};
+
+export default SearchProvider;
