dacda75441cbee8e2512eec9ce927c833c30e4f4
Mahendra <mahendrabrowny@gmail.com>
2022-08-28 23:00:55 +0530

    dashboard null for now

--- frontend/src/components/Admin/Dashboard/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,36 +1,7 @@
 import React from "react";
-import MovieUpload from "../Upload/MovieUpload";
-
-let timeoutId;
-const debounce = (func, delay) => {
-  return (...args) => {
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
 
 const Dashboard = () => {
-  const search = (value) => {
-    console.log(value);
-  };
-
-  const debounceSeach = debounce(search, 500);
-
-  const handleChange = ({ target }) => {
-    debounceSeach(target.value);
-  };
-
-  return (
-    <div className="p-14">
-      <input
-        onChange={handleChange}
-        type="text"
-        className="border border-gray-500"
-      />
-    </div>
-  );
+  return null;
 };
 
 export default Dashboard;
