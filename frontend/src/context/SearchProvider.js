feee5a4bfe3625e90624bc03d2a077a330fa6318
Mahendra <mahendrabrowny@gmail.com>
2022-08-28 23:01:16 +0530

    debounce function moved inside context

--- frontend/src/context/SearchProvider.js
+++ frontend/src/context/SearchProvider.js
@@ -2,8 +2,18 @@ import React, { createContext } from "react";
 
 const SearchContext = createContext();
 
-const SearchProvider = () => {
-  return <div>SearchProvider</div>;
+let timeoutId;
+const debounce = (func, delay) => {
+  return (...args) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const SearchProvider = ({ children }) => {
+  return <SearchContext.Provider> {children} </SearchContext.Provider>;
 };
 
 export default SearchProvider;
