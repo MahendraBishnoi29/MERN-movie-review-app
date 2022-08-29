ee55cdd5b970682ec3bb5fbdb26730d305ce0aee
Mahendra <mahendrabrowny@gmail.com>
2022-08-29 09:34:44 +0530

    custom hook for searching actor

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
 import { NotificationContext } from "../context/NotificationProvider";
+import { SearchContext } from "../context/SearchProvider";
 import { ThemeContext } from "../context/ThemeProvider";
 
 export const useTheme = () => useContext(ThemeContext);
@@ -8,3 +9,5 @@ export const useTheme = () => useContext(ThemeContext);
 export const useNotification = () => useContext(NotificationContext);
 
 export const useAuth = () => useContext(AuthContext);
+
+export const useSearch = () => useContext(SearchContext);
