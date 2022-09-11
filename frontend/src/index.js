e1e6528b7d8ac68933dd4b5779e5c4b9f030c533
Mahendra <mahendrabrowny@gmail.com>
2022-09-11 16:55:45 +0530

    search provider was on wrong place

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,23 +5,20 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import ContextProviders from "./context";
 import { ToastContainer } from "react-toastify";
-import SearchProvider from "./context/SearchProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <SearchProvider>
-        <ContextProviders>
-          <App />
-          <ToastContainer
-            position="top-center"
-            autoClose={3000}
-            newestOnTop={false}
-            closeOnClick
-          />
-        </ContextProviders>
-      </SearchProvider>
+      <ContextProviders>
+        <App />
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          newestOnTop={false}
+          closeOnClick
+        />
+      </ContextProviders>
     </BrowserRouter>
   </React.StrictMode>
 );
