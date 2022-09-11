947279d45bc9a0f0d3dacebddb0bf79be1fc57b1
Mahendra <mahendrabrowny@gmail.com>
2022-09-11 16:59:08 +0530

    import search provider

--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import AuthProvider from "./AuthProvider";
+import SearchProvider from "./SearchProvider";
 import ThemeProvider from "./ThemeProvider";
 
 const ContextProviders = ({ children }) => {
   return (
-    <AuthProvider>
-      <ThemeProvider>{children}</ThemeProvider>
-    </AuthProvider>
+    <SearchProvider>
+      <AuthProvider>
+        <ThemeProvider>{children}</ThemeProvider>
+      </AuthProvider>
+    </SearchProvider>
   );
 };
 
