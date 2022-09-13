a70ac3f5660dc9339c50c2a87c22307589a24943
Mahendra <mahendrabrowny@gmail.com>
2022-09-13 13:46:04 +0530

    refactor ♻

--- frontend/src/components/Admin/Dashboard/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,26 +1,11 @@
+import AppInfoBox from "../../Shared/AppInfoBox";
+
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-3 gap-5 my-5">
-      <div className="bg-white shadow-lg dark:shadow-lg dark:bg-secondary p-5 rounded">
-        <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
-          Total Uploads
-        </h1>
-        <p className="text-xl text-primary dark:text-white">100</p>
-      </div>
-
-      <div className="bg-white shadow-lg dark:bg-secondary p-5 rounded">
-        <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
-          Total Reviews
-        </h1>
-        <p className="text-xl text-primary dark:text-white">100</p>
-      </div>
-
-      <div className="bg-white shadow-lg dark:bg-secondary p-5 rounded">
-        <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
-          Total Users
-        </h1>
-        <p className="text-xl text-primary dark:text-white">100</p>
-      </div>
+      <AppInfoBox title="Total Uploads" subtitle="100" />
+      <AppInfoBox title="Total Reviews" subtitle="100" />
+      <AppInfoBox title="Total Users" subtitle="50" />
     </div>
   );
 };
