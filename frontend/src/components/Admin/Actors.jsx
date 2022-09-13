410c2405cbc98ebab4c5b0c4cb87ac451a1c1e0b
Mahendra <mahendrabrowny@gmail.com>
2022-09-13 16:12:16 +0530

    hover effects

--- frontend/src/components/Admin/Actors.jsx
+++ frontend/src/components/Admin/Actors.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { useState } from "react";
+import { BsBoxArrowDownRight, BsPencilSquare, BsTrash } from "react-icons/bs";
 
 const Actors = () => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const onMouseEnter = () => {
+    setShowOptions(true);
+  };
+
+  const onMouseLeave = () => {
+    setShowOptions(false);
+  };
+
   return (
     <div className="grid grid-cols-4 gap-3 my-5">
       <div className="bg-white shadow-lg dark:shadow-lg dark:bg-secondary h-20 rounded overflow-hidden">
-        <div className="flex cursor-pointer">
+        <div
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+          className="flex cursor-pointer relative"
+        >
           <img
             src="https://m.media-amazon.com/images/M/MV5BZTBiYmNkMGUtMzgzOC00YmZmLThiZDgtOTQxNzMxYmMwNmJiXkEyXkFqcGdeQXVyMTg1NjAxOQ@@._V1_UY1200_CR114,0,630,1200_AL_.jpg"
             alt=""
@@ -19,6 +35,17 @@ const Actors = () => {
               Laboriosam, maiores.
             </p>
           </div>
+
+          {showOptions ? (
+            <div className="absolute inset-0 bg-primary bg-opacity-25 backdrop-blur-sm">
+              <button className="" type="button">
+                <BsTrash />
+              </button>
+              <button className="" type="button">
+                <BsPencilSquare />
+              </button>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
