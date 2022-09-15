cd2e58796fcb0cd2038dd02aa40728663a5e25dc
Mahendra <mahendrabrowny@gmail.com>
2022-09-15 21:26:51 +0530

    fetching movies from DB 🔑

--- frontend/src/components/Admin/Movie/Movies.jsx
+++ frontend/src/components/Admin/Movie/Movies.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import { getMovies } from "../../../api/movie/movie";
+import { toast } from "react-toastify";
+import MovieListItem from "./MovieListItem";
+import { useEffect } from "react";
+
+const limit = 10;
+const currentPageNo = 0;
 
 const Movies = () => {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async (pageNo) => {
+    const { movies, error } = await getMovies(pageNo, limit);
+
+    if (error) return toast.error(error.message);
+    console.log(movies);
+    setMovies([...movies]);
+  };
+
+  useEffect(() => {
+    fetchMovies(currentPageNo);
+  }, []);
+
+  return (
+    <div className="space-y-3 p-5">
+      {movies.map((movie) => (
+        <MovieListItem key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 };
 
 export default Movies;
