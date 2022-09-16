0b6853e33dda7c6df7f3795571c311224bb5167f
Mahendra <mahendrabrowny@gmail.com>
2022-09-16 16:01:34 +0530

    go to next page functionality done

--- frontend/src/components/Admin/Movie/Movies.jsx
+++ frontend/src/components/Admin/Movie/Movies.jsx
@@ -4,19 +4,37 @@ import { getMovies } from "../../../api/movie/movie";
 import { toast } from "react-toastify";
 import MovieListItem from "./MovieListItem";
 import { useEffect } from "react";
+import NextPrevBtn from "../Actors/NextPrevBtn";
 
 const limit = 10;
-const currentPageNo = 0;
+let currentPageNo = 0;
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [reachedToEnd, setReachedToEnd] = useState(false);
 
   const fetchMovies = async (pageNo) => {
     const { movies, error } = await getMovies(pageNo, limit);
-
     if (error) return toast.error(error.message);
-    console.log(movies);
+
+    if (!movies?.length) {
+      currentPageNo = pageNo - 1;
+      return setReachedToEnd(true);
+    }
+
     setMovies([...movies]);
+  };
+
+  // Next Page
+  const onNext = () => {
+    if (reachedToEnd) return;
+    currentPageNo += 1;
+    fetchMovies(currentPageNo);
+  };
+
+  // Prev Page
+  const onPrev = () => {
+    if (reachedToEnd) return;
   };
 
   useEffect(() => {
@@ -28,6 +46,7 @@ const Movies = () => {
       {movies.map((movie) => (
         <MovieListItem key={movie.id} movie={movie} />
       ))}
+      <NextPrevBtn onNext={onNext} onPrev={onPrev} />
     </div>
   );
 };
