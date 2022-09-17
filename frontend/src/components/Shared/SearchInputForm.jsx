3e0099262ad96d5d3af7aefc39fc6ffa1b6ce37e
Mahendra <mahendrabrowny@gmail.com>
2022-09-17 18:11:32 +0530

    state handling and submit frm function

--- frontend/src/components/Shared/SearchInputForm.jsx
+++ frontend/src/components/Shared/SearchInputForm.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchInputForm = ({ placeholder }) => {
+const SearchInputForm = ({ placeholder, onSubmit }) => {
+  const [value, setValue] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <input
+        value={value}
+        onChange={({ target }) => setValue(target.value)}
         type="text"
         placeholder={placeholder}
         className="border-2 transition bg-transparent rounded text-lg p-1 outline-none dark:border-dark-subtle border-light-subtle dark:focus:border-white 
