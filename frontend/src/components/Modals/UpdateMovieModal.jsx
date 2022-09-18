0b8265f2c866fb63a297908b08445c8f868e6699
Mahendra <mahendrabrowny@gmail.com>
2022-09-18 17:23:22 +0530

    update movie function completed 🔥

--- frontend/src/components/Modals/UpdateMovieModal.jsx
+++ frontend/src/components/Modals/UpdateMovieModal.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { updateMovie } from "../../api/movie/movie";
 import MovieForm from "../Admin/Movie/MovieForm";
 import ModalContainer from "./ModalContainer";
 
-const UpdateMovieModal = ({ visible, initialState }) => {
+const UpdateMovieModal = ({ visible, onClose, onSuccess, initialState }) => {
+  const [busy, setBusy] = useState(false);
+
+  const handleSubmit = async (data) => {
+    setBusy(true);
+    const { error, movie, message } = await updateMovie(initialState.id, data);
+    setBusy(false);
+    if (error) return toast.error(error);
+    toast.success(message);
+    onSuccess(movie);
+    onClose();
+  };
+
   return (
     <ModalContainer visible={visible}>
-      <MovieForm btnTitle="Update" initialState={initialState} />
+      <MovieForm
+        btnTitle="Update"
+        initialState={initialState}
+        onSubmit={!busy ? handleSubmit : null}
+        busy={busy}
+      />
     </ModalContainer>
   );
 };
