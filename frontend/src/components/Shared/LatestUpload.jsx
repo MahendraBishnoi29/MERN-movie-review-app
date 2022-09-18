3f56ee444babda669629a3464a5812da537df6a4
Mahendra <mahendrabrowny@gmail.com>
2022-09-18 19:29:25 +0530

    show recent uploads on Dashboard ✔

--- frontend/src/components/Shared/LatestUpload.jsx
+++ frontend/src/components/Shared/LatestUpload.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { getMovies } from "../../api/movie/movie";
 import MovieListItem from "../Admin/Movie/MovieListItem";
 
+const pageNo = 0;
+const limit = 5;
+
 const LatestUpload = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchLatestUploads = async () => {
+    const { error, movies } = await getMovies(pageNo, limit);
+    if (error) return toast.error(error);
+    setMovies([...movies]);
+  };
+
+  useEffect(() => {
+    fetchLatestUploads();
+  }, []);
+
   return (
     <div className="bg-white shadow-lg dark:shadow dark:bg-secondary p-5 rounded col-span-2">
       <h1 className="font-semibold text-2xl mb-2 text-primary dark:text-white">
         Recent Uploads
       </h1>
 
-      <MovieListItem
-        movie={{
-          poster:
-            "https://www.economist.com/img/b/1190/670/90/sites/default/files/images/2015/09/blogs/economist-explains/code2.png",
-          title: "lorem ipsum dolor sit amet.",
-          status: "public",
-          genres: ["Action", "Sci-Fi"],
-        }}
-      />
+      <div className="space-y-3">
+        {movies.map((movie) => {
+          return <MovieListItem key={movie.id} movie={movie} />;
+        })}
+      </div>
     </div>
   );
 };
