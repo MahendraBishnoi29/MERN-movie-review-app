f218e1f39164057a5601f4c3dfa1f7a41d3a62c7
Mahendra <mahendrabrowny@gmail.com>
2022-09-18 16:47:48 +0530

    some route changes 🤔

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -2,17 +2,20 @@ const express = require("express");
 const {
   uploadTrailer,
   createMovie,
-  updateMovieWithoutPoster,
-  updateMovieWithPoster,
   deleteMovie,
   getMovies,
   getMovieForUpdate,
+  updateMovie,
 } = require("../controllers/movie");
 const router = express.Router();
 const { IsAuth, isAdmin } = require("../middlewares/authMiddlware");
 const { parseData } = require("../middlewares/helper");
 const { uploadVideo, uploadImage } = require("../middlewares/multer");
-const { validateMovie, validate } = require("../middlewares/validator");
+const {
+  validateMovie,
+  validate,
+  validateTrailer,
+} = require("../middlewares/validator");
 
 router.post(
   "/upload-trailer",
@@ -29,29 +32,30 @@ router.post(
   uploadImage.single("poster"),
   parseData,
   validateMovie,
+  validateTrailer,
   validate,
   createMovie
 );
 
-router.patch(
-  "/update-movie-without-poster/:movieId",
-  IsAuth,
-  isAdmin,
-  // parseData,
-  validateMovie,
-  validate,
-  updateMovieWithoutPoster
-);
+// router.patch(
+//   "/update-movie-without-poster/:movieId",
+//   IsAuth,
+//   isAdmin,
+//   // parseData,
+//   validateMovie,
+//   validate,
+//   updateMovieWithoutPoster
+// );
 
 router.patch(
-  "/update-movie-with-poster/:movieId",
+  "/update-movie/:movieId",
   IsAuth,
   isAdmin,
   uploadImage.single("poster"),
   parseData,
   validateMovie,
   validate,
-  updateMovieWithPoster
+  updateMovie
 );
 
 router.delete("/delete-movie/:movieId", IsAuth, isAdmin, deleteMovie);
