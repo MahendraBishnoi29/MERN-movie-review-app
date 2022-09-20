56026f12d408d09a1f4e46a130b59d3519000ece
Mahendra <mahendrabrowny@gmail.com>
2022-09-20 10:06:46 +0530

    wrapping context index file with movieProvider 🛠

--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import AuthProvider from "./AuthProvider";
+import MoviesProvider from "./Movie/MovieProvider";
 import SearchProvider from "./SearchProvider";
 import ThemeProvider from "./ThemeProvider";
 
 const ContextProviders = ({ children }) => {
   return (
     <SearchProvider>
-      <AuthProvider>
-        <ThemeProvider>{children}</ThemeProvider>
-      </AuthProvider>
+      <MoviesProvider>
+        <AuthProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </AuthProvider>
+      </MoviesProvider>
     </SearchProvider>
   );
 };
