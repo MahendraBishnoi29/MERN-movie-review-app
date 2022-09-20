d60cf97342a6031d28ed1d95cf5af3a14fba1d9c
Mahendra <mahendrabrowny@gmail.com>
2022-09-20 17:20:25 +0530

    route for updating review 👍🏻

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -1,9 +1,10 @@
-const { addReview } = require("../controllers/review");
+const { addReview, updateReview } = require("../controllers/review");
 const { IsAuth } = require("../middlewares/authMiddlware");
 const { validateRatings, validate } = require("../middlewares/validator");
 
 const router = require("express").Router();
 
 router.post("/add/:movieId", IsAuth, validateRatings, validate, addReview);
+router.patch("/:reviewId", IsAuth, validateRatings, validate, updateReview);
 
 module.exports = router;
