1d732602befd1a073be5f4dc9c8063a2b48e594d
Mahendra <mahendrabrowny@gmail.com>
2022-09-21 09:17:58 +0530

    route for fetching all reviews for specific movie 🔍

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -2,6 +2,7 @@ const {
   addReview,
   updateReview,
   removeReview,
+  getReviewByMovie,
 } = require("../controllers/review");
 const { IsAuth } = require("../middlewares/authMiddlware");
 const { validateRatings, validate } = require("../middlewares/validator");
@@ -11,5 +12,6 @@ const router = require("express").Router();
 router.post("/add/:movieId", IsAuth, validateRatings, validate, addReview);
 router.patch("/:reviewId", IsAuth, validateRatings, validate, updateReview);
 router.delete("/:reviewId", IsAuth, removeReview);
+router.get("/get-reviews-by-movie/:movieId", getReviewByMovie);
 
 module.exports = router;
