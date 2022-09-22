c4ff10cd719ff7abaa8e946e28fc5fac62c28c36
Mahendra <mahendrabrowny@gmail.com>
2022-09-22 14:09:11 +0530

    removed movie route from review routes

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -3,7 +3,6 @@ const {
   updateReview,
   removeReview,
   getReviewByMovie,
-  getLatestUploads,
 } = require("../controllers/review");
 const { IsAuth } = require("../middlewares/authMiddlware");
 const { validateRatings, validate } = require("../middlewares/validator");
@@ -14,8 +13,5 @@ router.post("/add/:movieId", IsAuth, validateRatings, validate, addReview);
 router.patch("/:reviewId", IsAuth, validateRatings, validate, updateReview);
 router.delete("/:reviewId", IsAuth, removeReview);
 router.get("/get-reviews-by-movie/:movieId", getReviewByMovie);
-
-// For Normal Users
-router.get("/latest-uploads", getLatestUploads);
 
 module.exports = router;
