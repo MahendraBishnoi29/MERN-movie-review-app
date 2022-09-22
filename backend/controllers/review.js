e90bf31de405f80b8b2e44abb8cc7eb8f50186f9
Mahendra <mahendrabrowny@gmail.com>
2022-09-22 14:10:13 +0530

    movie function moved to review 👍🏻

--- backend/controllers/review.js
+++ backend/controllers/review.js
@@ -108,31 +108,9 @@ const getReviewByMovie = async (req, res) => {
   res.json({ reviews });
 };
 
-// GET LATEST UPLOADS
-const getLatestUploads = async (req, res) => {
-  const { limit = 5 } = req.query;
-
-  const results = await Movie.find({ status: "public" })
-    .sort("-createdAt")
-    .limit(parseInt(limit));
-
-  const movies = results.map((m) => {
-    return {
-      id: m._id,
-      title: m.title,
-      poster: m.poster?.url,
-      trailer: m.trailer?.url,
-      storyLine: m.storyLine,
-    };
-  });
-
-  res.json({ movies });
-};
-
 module.exports = {
   addReview,
   updateReview,
   removeReview,
   getReviewByMovie,
-  getLatestUploads,
 };
