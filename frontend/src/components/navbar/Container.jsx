601b7a350521ec9ae3df6b5a4d76d6b91e33e0a6
Mahendra <mahendrabrowny@gmail.com>
2022-09-23 18:52:32 +0530

    UI fix 🐛

--- frontend/src/components/navbar/Container.jsx
+++ frontend/src/components/navbar/Container.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Container = ({ children, className }) => {
   return (
-    <div className={"max-w-screen-xl mx-auto px-2 xl:p-0 " + className}>
-      {children}
-    </div>
+    <div className={"max-w-screen-xl mx-auto " + className}>{children}</div>
   );
 };
 
