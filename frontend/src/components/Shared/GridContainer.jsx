74d43332b5f84a5f337376e3ecde258561f6f18f
Mahendra <mahendrabrowny@gmail.com>
2022-09-23 09:02:02 +0530

    grid component for reusability ♻

---
+++ frontend/src/components/Shared/GridContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const GridContainer = ({ children, className }) => {
+  return (
+    <div
+      className={
+        "grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 gap-3 " + className
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
+export default GridContainer;
