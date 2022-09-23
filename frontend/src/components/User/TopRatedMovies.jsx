6a69db875c1a3e598a77259db44b7ff1e82527d6
Mahendra <mahendrabrowny@gmail.com>
2022-09-23 13:56:38 +0530

    file path changed

--- frontend/src/components/User/TopRatedMovies.jsx
+++ frontend/src/components/User/TopRatedMovies.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { AiFillStar } from "react-icons/ai";
 import { toast } from "react-toastify";
 import { getTopRatedMovies } from "../../api/movie/movie";
-import GridContainer from "../Shared/GridContainer";
 import MovieList from "../Shared/Movie List/MovieList";
 
-const TopRated = () => {
+const TopRatedMovies = () => {
   const [movies, setMovies] = useState([]);
 
   const fetchMovies = async () => {
@@ -22,4 +20,4 @@ const TopRated = () => {
   return <MovieList movies={movies} title="Viewers Choice (Movies)" />;
 };
 
-export default TopRated;
+export default TopRatedMovies;
