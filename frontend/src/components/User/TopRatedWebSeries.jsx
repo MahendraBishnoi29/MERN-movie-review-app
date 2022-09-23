7bb69cfe3e629f6c9d73e3f1439e553df3c5a16c
Mahendra <mahendrabrowny@gmail.com>
2022-09-23 16:00:51 +0530

    fetching top rated web-series

--- frontend/src/components/User/TopRatedWebSeries.jsx
+++ frontend/src/components/User/TopRatedWebSeries.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { getTopRatedMovies } from "../../api/movie/movie";
+import MovieList from "../Shared/Movie List/MovieList";
 
 const TopRatedWebSeries = () => {
-  return <div>TopRatedWebSeries</div>;
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const { error, movies } = await getTopRatedMovies("Web Series");
+    if (error) return toast.error(error);
+    setMovies([...movies]);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return <MovieList movies={movies} title="Web Series" />;
 };
 
 export default TopRatedWebSeries;
