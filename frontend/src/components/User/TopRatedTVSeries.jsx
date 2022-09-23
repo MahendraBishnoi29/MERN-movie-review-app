c32fa4e8386481cb5677011a0e8e654beb960873
Mahendra <mahendrabrowny@gmail.com>
2022-09-23 17:30:35 +0530

    component for fetching TV series 👍🏻

---
+++ frontend/src/components/User/TopRatedTVSeries.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { getTopRatedMovies } from "../../api/movie/movie";
+import MovieList from "../Shared/MovieList/MovieList";
+
+const TopRatedTVSeries = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const { error, movies } = await getTopRatedMovies("TV Series");
+    if (error) return toast.error(error);
+    setMovies([...movies]);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return <MovieList movies={movies} title="TV Series" />;
+};
+
+export default TopRatedTVSeries;
