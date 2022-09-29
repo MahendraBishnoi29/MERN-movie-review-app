f9e2b8de7babb75ec532c27a418cfcf5ad208454
Mahendra <mahendrabrowny@gmail.com>
2022-09-29 21:08:28 +0530

    rendering movie info

--- frontend/src/components/SingleMoviePage/SingleMoviePage.jsx
+++ frontend/src/components/SingleMoviePage/SingleMoviePage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { getSingleMovie } from "../../api/movie/movie";
 
 const SingleMoviePage = () => {
-  return <div>SingleMoviePage</div>;
+  const [movie, setMovie] = useState({});
+  const [ready, setReady] = useState(false);
+
+  const { movieId } = useParams();
+
+  const fetchMovie = async () => {
+    const { error, movie } = await getSingleMovie(movieId);
+    if (error) return toast.error(error + "Error Fetching Single Movie");
+    setMovie(movie);
+  };
+
+  useEffect(() => {
+    if (movieId) fetchMovie();
+  }, [movieId]);
+
+  return <div className="text-2xl">{movie?.title}</div>;
 };
 
 export default SingleMoviePage;
