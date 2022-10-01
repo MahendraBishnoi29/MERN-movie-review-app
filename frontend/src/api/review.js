e7129d5e06fe013cbbf476be0f8aecd4e8b3df07
Mahendra <mahendrabrowny@gmail.com>
2022-10-01 18:27:37 +0530

    get reviews by movie function for frontend

--- frontend/src/api/review.js
+++ frontend/src/api/review.js
@@ -1,6 +1,7 @@
 import client from "./client";
 import { getToken } from "../utils/helper";
 
+// ADD REVIEW
 export const addReview = async (movieId, reviewData) => {
   const token = getToken();
   try {
@@ -12,5 +13,16 @@ export const addReview = async (movieId, reviewData) => {
     return data;
   } catch (error) {
     console.log("Error in Add Review Method" + error.message);
+  }
+};
+
+// GET REVIEW BY MOVIE
+export const getReviewsByMovie = async (movieId) => {
+  try {
+    const { data } = await client(`/review/get-reviews-by-movie/${movieId}`);
+
+    return data;
+  } catch (error) {
+    console.log("Error in Get Review for Movie" + error.message);
   }
 };
