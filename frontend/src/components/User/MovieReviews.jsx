a6fb42ba7ac15b003b3e0f9c0f7fe82f362e8d97
Mahendra <mahendrabrowny@gmail.com>
2022-10-01 18:32:48 +0530

    fetching reviews from DB 🌍

--- frontend/src/components/User/MovieReviews.jsx
+++ frontend/src/components/User/MovieReviews.jsx
@@ -1,8 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getReviewsByMovie } from "../../api/review";
 import Container from "../Navbar/Container";
 import CustomButtonLink from "../Shared/CustomButtonLink";
+import RatingStar from "../Shared/RatingStar";
+
+const getNameInitial = (name = "") => {
+  return name[0].toUpperCase();
+};
 
 const MovieReviews = () => {
+  const [movieReviews, setMovieReviews] = useState([]);
+
+  const { movieId } = useParams();
+
+  const fetchReviews = async () => {
+    const { reviews } = await getReviewsByMovie(movieId);
+    setMovieReviews([...reviews]);
+  };
+
+  useEffect(() => {
+    if (movieId) fetchReviews();
+  }, [movieId]);
+
   return (
     <div className="dark:bg-primary bg-white min-h-screen pb-10">
       <Container className="xl:px-0 px-2 p-8">
@@ -13,10 +36,33 @@ const MovieReviews = () => {
             </span>
             This is Title
           </h1>
-
           <CustomButtonLink label="Find My Review" />
         </div>
+
+        <div className="space-y-3 mt-4">
+          {movieReviews.map((review) => {
+            return <ReviewCard review={review} key={review.id} />;
+          })}
+        </div>
       </Container>
+    </div>
+  );
+};
+
+const ReviewCard = ({ review }) => {
+  const { owner, content, rating } = review;
+  return (
+    <div className="flex space-x-3">
+      <div className="flex items-center justify-center w-14 h-14 rounded-full bg-light-subtle dark:bg-dark-subtle text-xl select-none">
+        {getNameInitial(owner.name)}
+      </div>
+      <div className="">
+        <h1 className="dark:text-white text-secondary font-semibold text-lg">
+          {owner.name}
+        </h1>
+        <RatingStar rating={rating} />
+        <p className="text-light-subtle dark:text-dark-subtle">{content}</p>
+      </div>
     </div>
   );
 };
