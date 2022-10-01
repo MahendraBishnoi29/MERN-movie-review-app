6a8d06859221d786cdc2d8e549d07667098bb101
Mahendra <mahendrabrowny@gmail.com>
2022-10-01 09:00:49 +0530

    rendering all info about directors 👍🏻

--- frontend/src/components/Modals/ProfileModal.jsx
+++ frontend/src/components/Modals/ProfileModal.jsx
@@ -1,11 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState } from "react";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { getActorProfile } from "../../api/actor";
 import ModalContainer from "./ModalContainer";
 
 const ProfileModal = ({ visible, profileId, onClose }) => {
   const [profile, setProfile] = useState({});
+
+  const fetchActorProfile = async () => {
+    const { error, actor } = await getActorProfile(profileId);
+    if (error) return toast.error("Error Fetching Actors Profile!");
+    setProfile(actor);
+  };
+
+  useEffect(() => {
+    if (profileId) fetchActorProfile();
+  }, [profileId]);
+
+  const { avatar, name, about } = profile;
+
   return (
     <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
-      <div className="p-5 rounded bg-white dark:bg-primary"></div>
+      <div className="w-72 p-5 flex flex-col items-center rounded bg-white dark:bg-primary space-y-3">
+        <img src={avatar} alt={name} className="w-28 h-28 rounded-full" />
+        <h1 className="dark:text-white text-primary font-semibold">{name}</h1>
+        <p className="dark:text-dark-subtle text-light-subtle">{about}</p>
+      </div>
     </ModalContainer>
   );
 };
