30f6d0d71a0c178791dfeca8e999088c6f88f8df
Mahendra <mahendrabrowny@gmail.com>
2022-10-01 21:49:38 +0530

    functionality for viewing the reviews ✔

--- frontend/src/components/User/MovieReviews.jsx
+++ frontend/src/components/User/MovieReviews.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import { getReviewsByMovie } from "../../api/review";
+import { useAuth } from "../../hooks";
 import Container from "../Navbar/Container";
 import CustomButtonLink from "../Shared/CustomButtonLink";
 import RatingStar from "../Shared/RatingStar";
@@ -14,12 +16,23 @@ const getNameInitial = (name = "") => {
 
 const MovieReviews = () => {
   const [movieReviews, setMovieReviews] = useState([]);
+  const [profileOwners, setProfileOwners] = useState(null);
 
   const { movieId } = useParams();
+  const { authInfo } = useAuth();
+  const profileId = authInfo.profile?.id;
 
   const fetchReviews = async () => {
-    const { reviews } = await getReviewsByMovie(movieId);
+    const { reviews, error } = await getReviewsByMovie(movieId);
+    if (error) return toast.error(error + "Error Fetching Reviews by Movie!");
     setMovieReviews([...reviews]);
+  };
+
+  const findProfileOwnerReview = () => {
+    if (profileOwners) return setProfileOwners(null);
+    const match = movieReviews.find((review) => review.owner.id === profileId);
+    if (!match) return toast.error("You don't have any review!");
+    setProfileOwners(match);
   };
 
   useEffect(() => {
@@ -36,25 +49,39 @@ const MovieReviews = () => {
             </span>
             This is Title
           </h1>
-          <CustomButtonLink label="Find My Review" />
+
+          {profileId ? (
+            <CustomButtonLink
+              label={profileOwners ? "View All" : "Find My Review"}
+              onClick={findProfileOwnerReview}
+            />
+          ) : null}
         </div>
 
-        <div className="space-y-3 mt-4">
-          {movieReviews.map((review) => {
-            return <ReviewCard review={review} key={review.id} />;
-          })}
-        </div>
+        {profileOwners ? (
+          <div className="mt-3">
+            <ReviewCard review={profileOwners} />
+          </div>
+        ) : (
+          <div className="space-y-3 mt-4">
+            {movieReviews.map((review) => {
+              return <ReviewCard review={review} key={review.id} />;
+            })}
+          </div>
+        )}
       </Container>
     </div>
   );
 };
 
 const ReviewCard = ({ review }) => {
+  if (!review) return null;
+
   const { owner, content, rating } = review;
   return (
     <div className="flex space-x-3">
       <div className="flex items-center justify-center w-14 h-14 rounded-full bg-light-subtle dark:bg-dark-subtle text-xl select-none">
-        {getNameInitial(owner.name)}
+        {getNameInitial(owner?.name)}
       </div>
       <div className="">
         <h1 className="dark:text-white text-secondary font-semibold text-lg">
