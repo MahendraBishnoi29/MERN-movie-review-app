ab8fd68ae3149459e13e8ab7adbc90a6269b32d9
Mahendra <mahendrabrowny@gmail.com>
2022-10-02 21:35:56 +0530

    getting total db docs from server

---
+++ frontend/src/api/admin.js
@@ -0,0 +1,17 @@
+import { getToken } from "../utils/helper";
+import client from "./client";
+
+export const getAppInfo = async () => {
+  const token = getToken();
+  try {
+    const { data } = await client("/admin/app-info", {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
