43650703114d7b4b7574c67091e8d90af7c0b975
Mahendra <mahendrabrowny@gmail.com>
2022-10-02 17:55:56 +0530

    edit review functionality done 👍🏻

--- frontend/src/components/Modals/Review/EditRatingModal.jsx
+++ frontend/src/components/Modals/Review/EditRatingModal.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import { editReview } from "../../../api/review";
 import RatingForm from "../../Form/RatingForm";
 import ModalContainer from "../ModalContainer";
 
 const EditRatingModal = ({ visible, initialState, onClose, onSuccess }) => {
+  const [busy, setBusy] = useState(false);
+
   const handleSubmit = async (data) => {
+    setBusy(true);
     const { error, message } = await editReview(initialState.id, data);
+    setBusy(false);
     if (error) return toast.error(error + " error while editing the review");
     toast.success(message);
     onClose();
@@ -16,7 +20,11 @@ const EditRatingModal = ({ visible, initialState, onClose, onSuccess }) => {
 
   return (
     <ModalContainer visible={visible} onClose={onClose} ignoreContainer>
-      <RatingForm initialState={initialState} onSubmit={handleSubmit} />
+      <RatingForm
+        busy={busy}
+        initialState={initialState}
+        onSubmit={handleSubmit}
+      />
     </ModalContainer>
   );
 };
