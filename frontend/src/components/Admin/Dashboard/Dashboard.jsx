0a1c0bd3890c318e2bb8b5a24cffad3c18bbecb3
Mahendra <mahendrabrowny@gmail.com>
2022-10-02 21:36:06 +0530

    rendering actual data

--- frontend/src/components/Admin/Dashboard/Dashboard.jsx
+++ frontend/src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,12 +1,32 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { getAppInfo } from "../../../api/admin";
 import AppInfoBox from "../../Shared/AppInfoBox";
 import LatestUpload from "../../Shared/LatestUpload";
 
 const Dashboard = () => {
+  const [appInfo, setAppInfo] = useState({
+    movieCount: 0,
+    reviewCount: 0,
+    userCount: 0,
+  });
+
+  const fetchAppInfo = async () => {
+    const { appInfo, error } = await getAppInfo();
+    if (error) return toast.error("Error Getting App Info" + error);
+    setAppInfo({ ...appInfo });
+  };
+
+  useEffect(() => {
+    fetchAppInfo();
+  }, []);
+
   return (
     <div className="grid grid-cols-3 gap-5 my-5 p-5">
-      <AppInfoBox title="Total Uploads" subtitle="100" />
-      <AppInfoBox title="Total Reviews" subtitle="100" />
-      <AppInfoBox title="Total Users" subtitle="50" />
+      <AppInfoBox title="Total Uploads" subtitle={appInfo.movieCount} />
+      <AppInfoBox title="Total Reviews" subtitle={appInfo.reviewCount} />
+      <AppInfoBox title="Total Users" subtitle={appInfo.userCount} />
 
       <LatestUpload />
     </div>
