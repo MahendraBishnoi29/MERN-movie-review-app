02539fa5c652d4f382f249619f26259847907e2f
Mahendra <mahendrabrowny@gmail.com>
2022-10-03 08:37:22 +0530

    convert review count helper method

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -61,3 +61,8 @@ export const getPoster = (posters = []) => {
 
   return posters[0];
 };
+
+export const convertReviewCount = (count = 0) => {
+  if (count <= 999) return count;
+  return parseFloat(count / 1000).toFixed(2) + "k";
+};
