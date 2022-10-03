5dc37032830559e280f7ccd845a80c5b04de0049
Mahendra <mahendrabrowny@gmail.com>
2022-10-03 08:18:55 +0530

    most rated movie route 📌

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,7 +1,8 @@
-const { getAppInfo } = require("../middlewares/admin");
+const { getAppInfo, getMostRatedMovie } = require("../controllers/admin");
 const { IsAuth, isAdmin } = require("../middlewares/authMiddlware");
 const router = require("express").Router();
 
 router.get("/app-info", IsAuth, isAdmin, getAppInfo);
+router.get("/most-rated", IsAuth, isAdmin, getMostRatedMovie);
 
 module.exports = router;
