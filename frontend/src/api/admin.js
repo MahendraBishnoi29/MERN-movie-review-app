c6cefefe165c8d6594e1bf3440fe2b2618c81e55
Mahendra <mahendrabrowny@gmail.com>
2022-10-03 08:38:20 +0530

    most rated controller function

--- frontend/src/api/admin.js
+++ frontend/src/api/admin.js
@@ -15,3 +15,18 @@ export const getAppInfo = async () => {
     console.log(error);
   }
 };
+
+export const getMostRated = async () => {
+  const token = getToken();
+  try {
+    const { data } = await client("/admin/most-rated", {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
