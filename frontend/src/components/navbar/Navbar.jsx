fd2eddacf8dec1878a633d863e19306aa65c9d61
Mahendra <mahendrabrowny@gmail.com>
2022-11-26 14:33:56 +0530

    organise imports

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import SearchInputForm from "../Shared/SearchInputForm";
+import Container from "./Container";
 import { BsFillSunFill } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth, useTheme } from "../../hooks";
-import SearchInputForm from "../Shared/SearchInputForm";
-import Container from "./Container";
 
 const Navbar = () => {
   const { toggleTheme } = useTheme();
